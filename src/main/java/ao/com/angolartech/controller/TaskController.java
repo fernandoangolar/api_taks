df7cee3e8838276a1c285e0a7e0410571215b386
Fernando Angolar <fernandoangolar581@gmail.com>
2024-10-11 10:37:57 +0100

    :lipstick: feat: Adicionado a classe TaskController

--- src/main/java/ao/com/angolartech/controller/TaskController.java
+++ src/main/java/ao/com/angolartech/controller/TaskController.java
@@ -1,2 +1,30 @@
-package ao.com.angolartech.controller;public class TaskController {
+package ao.com.angolartech.controller;
+
+import ao.com.angolartech.dto.TaskRequest;
+import ao.com.angolartech.dto.TaskResponse;
+import ao.com.angolartech.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/task")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> create(@RequestBody @Valid TaskRequest request) {
+
+        TaskResponse response = taskService.create(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
+    }
 }
