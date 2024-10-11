f457e21cef48af21d2f6836c1ff92ca684af70af
Fernando Angolar <fernandoangolar581@gmail.com>
2024-10-11 09:37:46 +0100

    :sparkles: feat: Classe de ModelMapperConfig e TaskMapper adicionado

--- src/main/java/ao/com/angolartech/mapper/ModelMapperConfig.java
+++ src/main/java/ao/com/angolartech/mapper/ModelMapperConfig.java
@@ -1,2 +1,14 @@
-package ao.com.angolartech.mapper;public class ModelMapperConfig {
+package ao.com.angolartech.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/ao/com/angolartech/mapper/TaskMapper.java
+++ src/main/java/ao/com/angolartech/mapper/TaskMapper.java
@@ -1,2 +1,25 @@
-package ao.com.angolartech.mapper;public interface TaskMapper {
+package ao.com.angolartech.mapper;
+
+
+import ao.com.angolartech.dto.TaskRequest;
+import ao.com.angolartech.dto.TaskResponse;
+import ao.com.angolartech.model.Task;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Task requestToEntity(TaskRequest request) {
+        return modelMapper.map(request, Task.class);
+    }
+
+    public TaskResponse entityToResponse(Task task) {
+        return modelMapper.map(task, TaskResponse.class);
+    }
+
 }
